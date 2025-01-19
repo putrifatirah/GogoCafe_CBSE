0dd8e3ce172182b6f64707a6860a8bb7d48e7e95
putrifatirah <putri.fatirahh@gmail.com>
2025-01-19 19:03:48 +0800

    delete orderhistory and notification

--- src/main/java/com/example/demo/repositories/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-    // Custom query methods (optional)
-}
--- src/main/java/com/example/demo/repositories/OrderHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.OrderHistory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderHistoryRepository extends MongoRepository<OrderHistory, String> {
-    // Custom query methods (optional)
-}
